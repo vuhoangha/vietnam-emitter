b712cb5ef30e05e22839c84601c3fee7d79cd648
Vu Hoang Ha <vuhoangha@Vus-Mac-mini.local>
2018-12-04 20:11:16 +0700

    change to export

--- index.js
+++ index.js
@@ -2,74 +2,78 @@ const uuidv4 = require('uuid/v4');
 const dicFunc = {};
 const dicPTC = {};
 
-const Emitter = {
-    getId: () => {
-        return uuidv4();
-    },
-    addListener: (event, id, cb) => {
-        if (!cb) return;
-        if (!dicFunc[event]) dicFunc[event] = [];
-        const idEvent = id || Emitter.getId();
-        if (dicFunc[event].find(item => item.id === idEvent)) return;
-        dicFunc[event].push({
-            id: idEvent,
-            func: cb
-        });
-        return idEvent;
-    },
-    emit: (event, data) => {
-        if (!dicFunc[event]) return;
-        dicFunc[event].map(item => item.func(data));
-    },
-    deleteListener: (event, idEvent) => {
-        if (!dicFunc[event]) return;
-        let index = -1;
-        for (let i = 0; i < dicFunc[event].length; i++) {
-            if (dicFunc[event][i].id === idEvent) {
-                index = i;
-                break;
-            }
-        }
-        if (index !== -1) dicFunc[event].splice(index, 1);
-    },
-    deleteEvent: event => {
-        delete dicFunc[event];
-    },
-    deleteByIdEvent: idEvent => {
-        if (!dicFunc) return;
-        Object.keys(dicFunc).map(event => {
-            Emitter.deleteListener(event, idEvent);
-        });
-    },
-    addChildPTC: (parentID, childID, func) => {
-        dicPTC[parentID] = dicPTC[parentID] || [];
-        if (dicPTC[parentID].find(a => a.childID === childID)) return;
-        dicPTC[parentID].push({
-            childID,
-            func
-        });
-    },
-    parentEmitPTC: (parentID, param = {}, isPromise) => {
-        return new Promise(resolve => {
-            if (!dicPTC[parentID]) return resolve();
-            const listResul = dicPTC[parentID].map(item => item.func(param));
-            return isPromise
-                ? Promise.all(listResul).then(resolve)
-                : resolve(listResul);
-        });
-    },
-    removeChildPTC: (parentID, childID) => {
-        if (!dicPTC || !dicPTC[parentID]) return;
+export function getId() {
+    return uuidv4();
+}
 
-        let index = -1;
-        for (let i = 0; i < dicPTC[parentID].length; i++) {
-            if (dicPTC[parentID][i].childID === childID) {
-                index = i;
-                break;
-            }
+export function addListener(event, id, cb) {
+    if (!cb) return;
+    if (!dicFunc[event]) dicFunc[event] = [];
+    const idEvent = id || getId();
+    if (dicFunc[event].find(item => item.id === idEvent)) return;
+    dicFunc[event].push({
+        id: idEvent,
+        func: cb
+    });
+    return idEvent;
+}
+
+export function emit(event, data) {
+    if (!dicFunc[event]) return;
+    dicFunc[event].map(item => item.func(data));
+}
+
+export function deleteListener(event, idEvent) {
+    if (!dicFunc[event]) return;
+    let index = -1;
+    for (let i = 0; i < dicFunc[event].length; i++) {
+        if (dicFunc[event][i].id === idEvent) {
+            index = i;
+            break;
         }
-        if (index !== -1) dicPTC[parentID].splice(index, 1);
     }
-};
+    if (index !== -1) dicFunc[event].splice(index, 1);
+}
 
-module.exports = Emitter;
+export function deleteEvent(event) {
+    delete dicFunc[event];
+}
+
+export function deleteByIdEvent(idEvent) {
+    if (!dicFunc) return;
+    Object.keys(dicFunc).map(event => {
+        deleteListener(event, idEvent);
+    });
+}
+
+export function addChildPTC(parentID, childID, func) {
+    dicPTC[parentID] = dicPTC[parentID] || [];
+    if (dicPTC[parentID].find(a => a.childID === childID)) return;
+    dicPTC[parentID].push({
+        childID,
+        func
+    });
+}
+
+export function parentEmitPTC(parentID, param = {}, isPromise) {
+    return new Promise(resolve => {
+        if (!dicPTC[parentID]) return resolve();
+        const listResul = dicPTC[parentID].map(item => item.func(param));
+        return isPromise
+            ? Promise.all(listResul).then(resolve)
+            : resolve(listResul);
+    });
+}
+
+export function removeChildPTC(parentID, childID) {
+    if (!dicPTC || !dicPTC[parentID]) return;
+
+    let index = -1;
+    for (let i = 0; i < dicPTC[parentID].length; i++) {
+        if (dicPTC[parentID][i].childID === childID) {
+            index = i;
+            break;
+        }
+    }
+    if (index !== -1) dicPTC[parentID].splice(index, 1);
+}
